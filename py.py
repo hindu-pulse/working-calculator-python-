21c689946e48586abcee8808202c6f009ac6b5f9
hindu_pulse <187662706+hindu-pulse@users.noreply.github.com>
2024-12-29 22:14:46 +0530

    Add files via upload

---
+++ py.py
@@ -0,0 +1,18 @@
+a=int(input("Enter a number a: "))
+b=int(input("Enter a number b: "))
+c=a+b   
+d=a-b
+e=a*b
+f=a/b
+g=a%b
+h=a**b
+i=a//b
+print("The sum of a and b is ",c)           
+print("The difference of a and b is ",d)
+print("The product of a and b is ",e)
+print("The division of a and b is ",f)
+print("The remainder of a and b is ",g)
+print("The power of a and b is ",h)
+print("The floor division of a and b is ",i)
+
+        
